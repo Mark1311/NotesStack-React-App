d430353faf452a44888d07130f8f311a5e71b6c2
Bittu Kumar <bk901277@gmail.com>
2025-05-15 22:23:24 +0530

    Add Backend Render Link and Extra File

--- Frontend/src/Utils/constants.js
+++ Frontend/src/Utils/constants.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:8000'
+// export const BASE_URL = 'http://localhost:8000' // For Localhost ke liye 
+export const BASE_URL = 'https://notesstack-react-app.onrender.com'
