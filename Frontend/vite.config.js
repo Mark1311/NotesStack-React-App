44a0479f3e665e10576c646e7de28315d7cec68d
Bittu Kumar <bk901277@gmail.com>
2025-05-16 23:11:04 +0530

    Deploye dist

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
+  base: '/', // Root path for deployment (ensure it's correct for your setup)
   plugins: [
     tailwindcss(),
   ],
-})
+});
